b4bb9fb665ff8bab4865004078411fd1fe563245
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2020-01-13 19:48:17 +0530

    fixes

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,6 @@ import * as vscode from "vscode";
 import * as path from "path";
 import * as fs from "fs";
 
-const cats = {
-  "Coding Cat": "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif",
-  "Compiling Cat": "https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif",
-  "Testing Cat": "https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif"
-};
-
 function getHTMLContent(context: vscode.ExtensionContext): string {
   let htmlText: string = "";
 
